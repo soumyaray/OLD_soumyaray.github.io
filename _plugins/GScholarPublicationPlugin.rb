f713918b59fcdb053e21950ac331049087cda996
Soumya Ray <soumya.ray@gmail.com>
2014-05-20 00:32:02 +0800

    plugin auto-collects pub info (all but year)

--- _plugins/GScholarPublicationPlugin.rb
+++ _plugins/GScholarPublicationPlugin.rb
@@ -1,4 +1,5 @@
 require 'gscholar'
+require 'haml'
 
 module Jekyll
 
@@ -11,13 +12,35 @@ module Jekyll
 
     def render(context)
       gpub = GScholarPub.new(@pub_id)
-      # "<tr>"\
-      # "<td>citations: #{gpub.cites}</td>"\
-      # "<td><a href=\"#{gpub.article_url}\">article link</a></td>"\
-      # "</tr>"
-      
-      "<div class=\"pub_url\"><a href=\"#{gpub.article_url}\">find it online</a></div>"\
-      "<div class=\"pub_cit\">cited #{gpub.cites} times</div>"
+
+      # gather all names
+      names_a = gpub.authors.map do |n|
+        n.last + ', ' + (n.first(n.size-1)).reduce('') { |names, n2| names+n2[0]+'.' }
+      end
+
+      names = ''
+      if names_a.length > 1 then
+        all_but_last = names_a.first(names_a.length-1).join(', ')
+        if names_a.length == 2 then append_s = 'and' else append_s = '&' end
+        names = all_but_last + ", #{append_s} " + names_a.last
+      else
+        names = names_a[0]
+      end
+
+      haml_code = Haml::Engine.new(%Q[
+%div.pub
+  %div.pub_ref
+    #{names} 2013. “#{gpub.title}”
+    %span.journal #{gpub.journal}
+    (#{gpub.volume}:#{gpub.issue}), pp. #{gpub.pages}.
+  %div.pub_url
+    %a{ :href => "#{gpub.article_url}" }
+      find it online
+  %div.pub_cit
+    cited #{gpub.cites} times
+      ])
+
+      haml_code.render
     end
   end
 
