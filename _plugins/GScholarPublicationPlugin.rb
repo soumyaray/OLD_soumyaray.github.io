fef2df2e1ebb444ef2b9ab0f3e7f4c955ed874b2
Soumya Ray <soumya.ray@gmail.com>
2014-06-05 23:55:16 +0800

    Switched from soumyaray/gscholar gem to soumyaray/gentle-scholar gem; Check for no cites information

--- _plugins/GScholarPublicationPlugin.rb
+++ _plugins/GScholarPublicationPlugin.rb
@@ -1,4 +1,5 @@
-require 'gscholar'
+#require 'gscholar'
+require 'gentle-scholar'
 require 'haml'
 
 module Jekyll
@@ -11,10 +12,10 @@ module Jekyll
     end
 
     def render(context)
-      gpub = GScholarPub.new(@pub_id)
+      gpub = GentleScholar::Publication.get_from_http(@pub_id)
 
       # gather all names
-      names_a = gpub.authors.map do |n|
+      names_a = gpub[:authors].map do |n|
         n.last + ', ' + (n.first(n.size-1)).reduce('') { |names, n2| names+n2[0]+'.' }
       end
 
@@ -27,17 +28,23 @@ module Jekyll
         names = names_a[0]
       end
 
+      if gpub[:cites]
+        cite_str = "cited #{gpub[:cites]} times"
+      else
+        cite_str = ""
+      end
+
       haml_code = Haml::Engine.new(%Q[
 %div.pub
   %div.pub_ref
-    #{names} 2013. “#{gpub.title}”
-    %span.journal #{gpub.journal}
-    (#{gpub.volume}:#{gpub.issue}), pp. #{gpub.pages}.
+    #{names} #{gpub[:date].year}. “#{gpub[:title]}”
+    %span.journal #{gpub[:journal]}
+    (#{gpub[:volume]}:#{gpub[:issue]}), pp. #{gpub[:pages]}.
   %div.pub_url
-    %a{ :href => "#{gpub.article_url}" }
+    %a{ :href => "#{gpub[:article_url]}" }
       find it online
   %div.pub_cit
-    cited #{gpub.cites} times
+    #{cite_str}
       ])
 
       haml_code.render
